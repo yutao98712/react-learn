58378fbc9d4e5efa979ee7ec1cea74e05444194c
yutao98712 <yutao98712@gmail.com>
2017-06-02 13:00:55 +0800

    second

--- src/component/list.js
+++ src/component/list.js
@@ -24,7 +24,7 @@ class Number extends Component {
         { id: 1, name: 'aaa' },
         { id: 2, name: 'bbb' },
         { id: 3, name: 'ccc' },
-        { id: 4, name: 'ddd' }
+        { id: 4, name: 'ttt' }
       ]
     };
     this.addUsers = this.addUsers.bind(this);
